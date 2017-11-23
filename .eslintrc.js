f71070e915f44ed0e81c9072b07b88b746bb7953
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2017-11-23 13:22:30 -0500

    Fix eslint to use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-    "extends": "google"
+    env: {
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'prettier'
+    ],
+    plugins: [
+        'prettier'
+    ],
+    rules: {
+        // Custom prettier rules
+        'prettier/prettier': [
+            'error', {
+                singleQuote: true, 
+                trailingComma: 'all',
+            }
+        ],
+        // Custom eslint rules
+        eqeqeq: ['error', 'always']
+    }
 };
